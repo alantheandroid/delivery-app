dd1b9bfa6d88a81e5fe0cb804088c40ae050a2c1
Alessio Ilardi <alessio.ilardi@gmail.com>
2024-06-06 09:56:08 +0200

    refactor: Update theme colors and breakpoints

--- src/theme.ts
+++ src/theme.ts
@@ -7,16 +7,20 @@ export type ThemeSpacings = keyof Theme["spacing"];
 
 export const theme = {
   colors: {
-    primary: "rgb(3, 54, 64)",
-    secondary: "rgb(228, 167, 22)",
-    white: "#ffffff",
-    black: "#000000",
+    primary: "#A5222D",
+    secondary: "#EFA32D",
+    accent: "#FFCC7C",
+    accentLight: "#FFF4E4",
+    white: "#FFFDFA",
+    black: "#36363",
   },
   breakpoints: {
-    sm: 640,
-    md: 768,
-    lg: 1024,
-    xl: 1280,
+    xxs: "320px",
+    xs: "480px",
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
   },
   spacing: {
     "0": "0px",
@@ -26,6 +30,12 @@ export const theme = {
     xl: "16px",
     xxl: "24px",
     xxxl: "32px",
+    xxxxl: "64px",
+  },
+  shadow: {
+    sm: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    md: "0 6px 12px rgba(0, 0, 0, 0.1)",
+    lg: "0 8px 16px rgba(0, 0, 0, 0.1)",
   },
 } as const;
 
@@ -35,9 +45,17 @@ export const antTheme: ThemeConfig = {
     colorPrimary: theme.colors.primary,
   },
   components: {
+    Button: {
+      borderRadius: 16,
+      defaultBg: theme.colors.accentLight,
+      defaultBorderColor: "transparent",
+      defaultColor: theme.colors.primary,
+      fontWeight: 600,
+    },
     Layout: {
-      headerBg: theme.colors.primary,
-      headerColor: theme.colors.secondary,
+      headerBg: theme.colors.white,
+      headerColor: theme.colors.primary,
+      headerHeight: 70,
     },
     Typography: {
       titleMarginBottom: theme.spacing[0],
