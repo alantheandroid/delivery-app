3e39316097dc21bc6b62a46988c92fdb833b34cd
Alessio Ilardi <alessio.ilardi@gmail.com>
2024-06-06 09:56:01 +0200

    refactor: Update global styles with Rubik font and responsive padding

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
+import Rubik from "./assets/fonts/Rubik/Rubik-Regular.ttf";
+import RubikBold from "./assets/fonts/Rubik/Rubik-Bold.ttf";
 
 export const GlobalStyle = createGlobalStyle`
+
+@font-face {
+  font-family: "Rubik";
+  src: url(${Rubik}) format("truetype");
+}
+
+@font-face {
+  font-family: "Rubik-Bold";
+  src: url(${RubikBold}) format("truetype");
+}
+
 *, *::before, *::after {
   box-sizing: border-box;
   margin: 0;
@@ -10,5 +23,24 @@ export const GlobalStyle = createGlobalStyle`
 html, body, #root {
   height: 100%;
   width: 100%;
+  font-family: "Rubik", sans-serif;
+  background-color: ${({ theme }) => theme.colors.accentLight};
+}
+
+
+#root {
+  padding: 0px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    padding: ${({ theme }) => theme.spacing.xxl};
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    padding: ${({ theme }) => theme.spacing.xxxl};
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    padding: 64px;
+  }
 }
 `;
