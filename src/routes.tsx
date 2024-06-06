662a9c08990b7eaf2bc04340102e85485b855a35
Alessio Ilardi <alessio.ilardi@gmail.com>
2024-06-06 09:56:15 +0200

    feat: Add route for RestaurantDetail page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import Home from "./pages/Home";
+import RestaurantDetail from "./pages/RestaurantDetail";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index path="/" element={<Home />} />
+        <Route path="/restaurant/:restId" element={<RestaurantDetail />} />
       </Route>
     </Routes>
   );
